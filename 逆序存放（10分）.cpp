9a8e641addeed14497ec3eed9cf28a864fd05f74
Coy Cheung <73786920+Stardust-Coy@users.noreply.github.com>
2020-11-02 22:45:21 +0800

    逆序存放（10分）
    5逆序存放（10分）
    题目内容：将一个由10个元素构成的int数组中的元素按逆序重新存放。
    
    输入：10个整数
    
    输出：按逆序重新存放后的数组元素（相邻元素之间用一个空格隔开）
    
    样例  输入：6 8 12 11 3 5 9 16 19 24            输出：24 19 16 9 5 3 11 12 8 6
    
    时间限制：500ms内存限制：32000kb

---
+++ 逆序存放（10分）.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int a[10];
+	for(int i=0;i<10;i++){
+		scanf("%d",&a[i]);
+	}
+	for(int i=9;i>0;i--){
+		printf("%d ",a[i]);
+	}
+	printf("%d\n",a[0]);
+	return 0;
+}
